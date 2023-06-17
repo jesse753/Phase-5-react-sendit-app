ea62bb210c9c414e77444f301917106b96122c38
jesse <jesse.kibaara@student.moringaschool.com>
2023-06-17 23:35:50 +0300

    app component added

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from './Map';
+
+const App = () => {
+  return (
+    <div style={{ height: '400px', width: '100%' }}>
+      <Map />
+    </div>
+  );
+};
+
+export default App;
